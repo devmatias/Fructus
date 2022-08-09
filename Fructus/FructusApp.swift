125c856f0942a2ac82f71f4357d371d3034e6d29
devmatias <devmatias@gmail.com>
2022-08-09 10:53:46 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Matias Correa Franco de Faria on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
